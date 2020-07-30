a36f0325c02cee2360adb6de03b1b4ff5c734bfa
gunesh <gunesh26@gmail.com>
2020-07-30 13:32:18 +0530

    1 file modified, 1 added, 1 deleted

--- Tap.js
+++ Tap.js
@@ -1,24 +1,22 @@
 import React, { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import ThemeContext from "./ThemeContext";
-import App from "./App";
+import Router from "./Router";
 
 const Tap = props => {
   const [style, setStyle] = useState("light");
   function toggleStyle() {
     setStyle(style => (style === "light" ? "dark" : "light"));
   }
-  
+
   return (
     <React.StrictMode>
-    <BrowserRouter>
-      <ThemeContext.Provider
-        value={{ style, toggleStyle }}
-      >
-        <App />
-      </ThemeContext.Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+      <BrowserRouter>
+        <ThemeContext.Provider value={{ style, toggleStyle }}>
+          <Router />
+        </ThemeContext.Provider>
+      </BrowserRouter>
+    </React.StrictMode>
   );
 };
 export default Tap;
